f6b89fb8bd8e5b51839a1c3b2caf72115774f03f
Thomas Schopinski <35452593+tomekhub@users.noreply.github.com>
2018-02-20 21:02:08 +0100

    added ProductService

--- src/main/java/com/packt/webstore/controller/ProductController.java
+++ src/main/java/com/packt/webstore/controller/ProductController.java
@@ -5,16 +5,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.packt.webstore.domain.repository.ProductRepository;
+import com.packt.webstore.service.ProductService;
 
 @Controller
+@RequestMapping("/products")
 public class ProductController {
 	@Autowired
-	private ProductRepository produktRepository;
-	@RequestMapping("/products")
+	private ProductService productService;
+	@RequestMapping
 	public String list(Model model) {
-		model.addAttribute("products", produktRepository.getAllProducts());
+		model.addAttribute("products", productService.getAllProducts());
 		return "products";
-		
+	}
+	@RequestMapping("/all")
+	public String allProducts(Model model) {
+		model.addAttribute("products", productService.getAllProducts());
+		return "products";
 	}
 }
